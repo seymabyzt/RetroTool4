e84c1f094f181b40020144b3e1a92de76e6cf74f
Hasan YASEN <ysnhasan1@gmail.com>
2024-08-06 16:29:41 +0300

    Farklı aşamalara geçmeyi sağlayan butonun disable olma durumu sağlandı.

---
+++ retro-tool-app/client/app/components/Atoms/StepButton.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Button } from "antd"
+import { useAppSelector } from "@/app/redux/store/store"
+
+const StepButton = ({ step, setStep }: { step: number, setStep: any }) => {
+
+    const [isDisabledButton, setIsDisabledButton] = useState(true)
+
+    const btnStyle: React.CSSProperties = {
+        color: '#fff',
+        fontWeight: "bold",
+        backgroundColor: isDisabledButton ? '#d9d9d9' : '#364d79',
+    }
+
+    const commentList1 = useAppSelector((state) => state.commentList.commentList1)
+    const commentList2 = useAppSelector((state) => state.commentList.commentList2)
+    const commentList3 = useAppSelector((state) => state.commentList.commentList3)
+
+    useEffect(() => {
+        if (commentList1.length > 0 || commentList2.length > 0 || commentList3.length > 0) {
+            setIsDisabledButton(false)
+        } else {
+            setIsDisabledButton(true)
+        }
+    }, [commentList1, commentList2, commentList3])
+
+    return (
+        <Button disabled={isDisabledButton} onClick={() => setStep(step + 1)} style={btnStyle}>
+            {step == 1 ? "Group & vote comments" : "Discuss and add action items"}
+        </Button>
+    )
+}
+
+export default StepButton
--- retro-tool-app/client/app/components/Navbar/Navbar.tsx
+++ retro-tool-app/client/app/components/Navbar/Navbar.tsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import { Button, Flex, Typography } from 'antd';
-
-const boxStyle: React.CSSProperties = {
-  width: '100%',
-  height: 60,
-  borderBottom: '1px solid #40a9ff',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: 10,
-  background: '#f0f5ff'
-};
-const btnStyle: React.CSSProperties = {
-  color: '#fff',
-  background: '#364d79',
-};
-
-const { Title } = Typography;
+import { Flex, Typography } from 'antd'
+import StepButton from '../Atoms/StepButton'
 
 const Navbar = ({ step, setStep }: { step: number, setStep: any }) => {
+
+  const { Title } = Typography
+
+  const boxStyle: React.CSSProperties = {
+    width: '100%',
+    height: 60,
+    borderBottom: '1px solid #40a9ff',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    background: '#f0f5ff'
+  }
+
   return (
     <Flex gap="middle" align="start" vertical>
-
       <Flex style={boxStyle}>
         <Title level={4}>RetroTool4</Title>
-        <Button onClick={() => setStep(step + 1)} style={btnStyle}>Group & Vote Comments</Button>
+        <StepButton step={step} setStep={setStep} />
       </Flex>
     </Flex>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
