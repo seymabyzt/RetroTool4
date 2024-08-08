33a36714d256da5b730ee39be3f8e76d9483d461
Hasan YASEN <ysnhasan1@gmail.com>
2024-08-08 22:08:43 +0300

    Tasarımda iyileştirme yapıldı, Sürükle bırak özelliği yalnızca 2. aşamada yapılabilir hale getirildi.

--- retro-tool-app/client/app/components/Atoms/CommentItem.tsx
+++ retro-tool-app/client/app/components/Atoms/CommentItem.tsx
@@ -1,39 +1,41 @@
 import { DeleteTwoTone, LikeTwoTone } from '@ant-design/icons'
 import HideInput from './HideInput'
 import { CommentItemProps } from '@/app/interfaces/interfaces'
-import { Flex } from 'antd'
-import {useDrag} from 'react-dnd'
+import { Flex, Card } from 'antd'
+import { useDrag } from 'react-dnd'
 import { useRef } from 'react'
 
 const CommentItem = ({ comment, userID, step, column, deleteCommentAndNotify, handleIncrementLike }: CommentItemProps) => {
-  const ref= useRef<HTMLDivElement>(null);
-  const[, dragRef] = useDrag({
+  const ref = useRef<HTMLDivElement>(null);
+  const [, dragRef] = useDrag({
     type: 'COMMENT_ITEM',
-    item: {comment, column},
+    item: { comment, column },
   });
   return (
-    <div ref={dragRef} style={{ borderBottom: "1px solid #9BB0C1", padding: "10px", margin: "10px 0" }}>
-      <Flex justify="space-between" align="center">
-        <div style={{ width: "100%", whiteSpace: "wrap", wordBreak: "break-word" }}>
-          {userID === comment.userID && step == 1 && (
-            <DeleteTwoTone onClick={() => deleteCommentAndNotify(comment.commentID,false)} />
-          )}
-          {step === 1
-            ? userID === comment.userID
-              ? comment.comment
-              : <HideInput />
-            : step === 2
-              ? comment.comment + " "
-              : comment.comment
-          }
-        </div>
-        <div>
-          {((step === 2 || step === 3) && column !== 'four') && comment.likeCount}
-          {step === 2 && column !== 'four' && (
-            <LikeTwoTone onClick={() => handleIncrementLike(comment.commentID)} />
-          )}
-        </div>
-      </Flex>
+    <div ref={step === 2 ? dragRef : null} style={{ margin: "10px 0", cursor: step === 2 ? "grabbing" : "default" }}>
+      <Card>
+        <Flex justify="space-between" align="center">
+          <Flex gap={5} style={{ width: "80%", whiteSpace: "wrap", wordBreak: "break-word" }}>
+            {userID === comment.userID && step == 1 && (
+              <DeleteTwoTone onClick={() => deleteCommentAndNotify(comment.commentID, false)} />
+            )}
+            {step === 1
+              ? userID === comment.userID
+                ? comment.comment
+                : <HideInput />
+              : step === 2
+                ? comment.comment + " "
+                : comment.comment
+            }
+          </Flex>
+          <Flex gap={5}>
+            <span style={{ fontWeight: "bold" }}>{((step === 2 || step === 3) && column !== 'four') && comment.likeCount}</span>
+            {step === 2 && column !== 'four' && (
+              <LikeTwoTone onClick={() => handleIncrementLike(comment.commentID)} />
+            )}
+          </Flex>
+        </Flex>
+      </Card>
     </div>
   )
 }
