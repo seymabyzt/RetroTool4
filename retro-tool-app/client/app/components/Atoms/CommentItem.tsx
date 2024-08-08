be41ff9aa895f2a78fe51371122199513ba37329
Hasan YASEN <ysnhasan1@gmail.com>
2024-08-08 18:54:23 +0300

    Drag and Drop componentleri eklendi.

--- retro-tool-app/client/app/components/Atoms/CommentItem.tsx
+++ retro-tool-app/client/app/components/Atoms/CommentItem.tsx
@@ -7,8 +7,8 @@ const CommentItem = ({ comment, userID, step, column, deleteCommentAndNotify, ha
   return (
     <div style={{ borderBottom: "1px solid #9BB0C1", padding: "10px", margin: "10px 0" }}>
       <Flex justify="space-between" align="center">
-        <div style={{ width: "100%" }}>
-          {userID === comment.userID && step !== 4 && (
+        <div style={{ width: "100%", whiteSpace: "wrap", wordBreak: "break-word" }}>
+          {userID === comment.userID && step == 1 && (
             <DeleteTwoTone onClick={() => deleteCommentAndNotify(comment.commentID)} />
           )}
           {step === 1
@@ -16,10 +16,12 @@ const CommentItem = ({ comment, userID, step, column, deleteCommentAndNotify, ha
               ? comment.comment
               : <HideInput />
             : step === 2
-              ? comment.comment + " " + comment.likeCount
-              : comment.comment}
+              ? comment.comment + " "
+              : comment.comment
+          }
         </div>
         <div>
+          {((step === 2 || step === 3) && column !== 'four') && comment.likeCount}
           {step === 2 && column !== 'four' && (
             <LikeTwoTone onClick={() => handleIncrementLike(comment.commentID)} />
           )}
---
+++ retro-tool-app/client/app/components/Atoms/Droppable.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {useDroppable} from '@dnd-kit/core';
+
+function Droppable(props: any) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: props.id,
+  });
+  const style = {
+    opacity: isOver ? 1 : 0.5,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Droppable
