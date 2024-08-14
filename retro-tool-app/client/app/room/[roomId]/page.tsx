586be7ed4d9dcc63eec44ab2a0c6265a16a913c1
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-08-14 13:05:23 +0300

    Update

--- retro-tool-app/client/app/room/[roomId]/page.tsx
+++ retro-tool-app/client/app/room/[roomId]/page.tsx
@@ -12,7 +12,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import StepDescription from "@/app/components/Atoms/StepDescription";
 
-const socket: Socket = io("https://retro-tool4.vercel.app");
+const socket: Socket = io("https://retro-tool4-gx1n8f34j-seyma-bayezits-projects.vercel.app/");
 
 const Room = ({ params }: any) => {
   const roomID = params.roomId;
