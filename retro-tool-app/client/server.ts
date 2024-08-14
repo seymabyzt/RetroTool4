bef6685850aa510433b129b449db3f7c12c90dfd
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-08-14 14:01:28 +0300

    Update for Vercel

--- retro-tool-app/client/server.ts
+++ retro-tool-app/client/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://retro-tool4-olqn.vercel.app",
+        origin: "https://retro-tool4.vercel.app/",
         methods: ["GET", "POST"],
     },
 })
