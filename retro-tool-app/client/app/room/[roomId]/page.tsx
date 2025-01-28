627111baab5087e5804094048a09ab8d9bd721eb
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2025-01-29 01:50:12 +0300

    update

--- retro-tool-app/client/app/room/[roomId]/page.tsx
+++ retro-tool-app/client/app/room/[roomId]/page.tsx
@@ -39,7 +39,7 @@ const Room = ({ params }: any) => {
     setUserID(storedUserID);
 
     if (!socket) {
-      socket = io("https://retrotool4server-production.up.railway.app/, http://localhost:8000"); 
+      socket = io("https://retrotool4server-production.up.railway.app/"); 
     }
 
     socket.emit("joinRoom", { roomID, userID: storedUserID });
