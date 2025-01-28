db435c9daafcc404440ec5c943d7089ccb45f293
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2025-01-29 01:42:01 +0300

    Update origin

--- retro-tool-app/client/app/room/[roomId]/page.tsx
+++ retro-tool-app/client/app/room/[roomId]/page.tsx
@@ -39,7 +39,7 @@ const Room = ({ params }: any) => {
     setUserID(storedUserID);
 
     if (!socket) {
-      socket = io("http://localhost:8000, https://retrotool4server.onrender.com"); 
+      socket = io("http://localhost:8000, retrotool4server.railway.internal"); 
     }
 
     socket.emit("joinRoom", { roomID, userID: storedUserID });
